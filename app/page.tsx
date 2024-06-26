88bb7a579aa8615b2f2c22499b9052d58de919f8
Husain Salah <husain.salah@gmail.com>
2024-06-25 21:14:43 -0400

    logo tweak

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,13 @@ export default function Home() {
     <section className="flex flex-col lg:flex-row">
       {/* second half */}
 
-      <section className="h-screen flex w-full flex-col justify-center items-center bg-[url('../public/images/splash-bg-img.png')]">
+      <section className="h-screen flex w-full flex-col justify-center items-center bg-[url('../public/images/splash-bg-img.png')] bg-cover">
         <Wrapper>
           <div className="p-10 lg:p-40">
             <center>
-              <p className="flex w-full flex-col align-center items-center text-2xl text-white p-2 tracking-widest">COMING SOON</p>
-              <Image src={logo} alt="Throat Logo" className="min-width-20 lg:p-10" />
-              <p className="flex w-full flex-col align-center items-center text-2xl text-white p-4">Where frontline artists meet global activists.</p>
+              <p className="flex w-full flex-col align-center items-center text-xl text-white p-2 tracking-widest">COMING SOON</p>
+              <Image src={logo} alt="Throat Logo" className="min-width-10 w-auto lg:p-20 m-0" />
+              <p className="flex w-full flex-col align-center items-center text-xl text-white p-4">Where frontline artists meet global activists</p>
             </center>
           </div>
           <div className="">
@@ -31,7 +31,7 @@ export default function Home() {
             </Button>
           </div>
           <div className="pt-40 pr-8 lg:pt-0 w-full text-right text-zinc-500">
-            <p>Photo by Morgana Wingard</p>
+            <p><i>Photo by Morgana Wingard</i></p>
           </div>
         </Wrapper>
 
