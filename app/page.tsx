60f91fd2b751cdbac2ff1035b3d0958591bab01f
Husain Salah <husain.salah@gmail.com>
2024-07-16 14:17:35 -0400

    Centered Image. Changed text size for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,13 @@ export default function Home() {
     <section className="flex flex-col lg:flex-row">
       {/* second half */}
 
-      <section className="h-screen flex w-full flex-col justify-center items-center bg-[url('../public/images/splash-bg-img.png')] bg-cover">
+      <section className="h-screen flex w-full flex-col justify-center items-center bg-[url('../public/images/splash-bg-img.png')] bg-cover bg-center">
         <Wrapper>
           <div className="p-10 lg:p-40">
             <center>
               <p className="flex w-full flex-col align-center items-center font-bold text-xl text-white p-2 tracking-[.5em]">COMING SOON</p>
               <Image src={logo} alt="Throat Logo" className="min-width-10 w-auto lg:p-10 m-0" />
-              <p className="flex w-full flex-col align-center items-center text-2xl text-white p-4">Frontline Artists | Global Activists | Positive Impact</p>
+              <p className="flex w-full flex-col align-center items-center text-base lg:text-2xl text-white p-4">Frontline Artists | Global Activists | Positive Impact</p>
             </center>
           </div>
 
